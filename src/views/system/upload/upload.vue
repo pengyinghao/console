7a8cf9f38980bdce7abdf267becf71e748ad9f24
pengyinghao <729220650@qq.com>
2024-06-18 16:54:10 +0800

    refactor(upload): condition optimize

--- src/views/system/upload/upload.vue
+++ src/views/system/upload/upload.vue
@@ -1,15 +1,9 @@
 <script setup lang="tsx">
-import { reactive, ref } from 'vue'
-import { PageContainer, Table, TableColumn } from '@/components'
+import { reactive } from 'vue'
+import { PageContainer, SearchOption, Table, TableColumn } from '@/components'
 import { Upload, fetchUploadInfos } from '@/service/api/system/upload'
 import { formatSize } from '@/utils'
 defineOptions({ name: 'Upload' })
-
-const reload = ref(true)
-const queryParams = reactive({ originalname: '' })
-const handleQuery = () => {
-    reload.value = true
-}
 
 const columns: TableColumn<Upload>[] = [
     { label: '原文件名', prop: 'originalname' },
@@ -27,21 +21,11 @@ const columns: TableColumn<Upload>[] = [
     { label: '拓展名', prop: 'ext', width: 90 },
     { label: '上传时间', prop: 'createTime', dateFormat: true, width: 160 }
 ]
+
+const options = reactive<SearchOption[]>([{ mode: 'input', label: '原文件名', field: 'originalname' }])
 </script>
 <template>
     <PageContainer>
-        <template #header>
-            <el-form inline class="search-form" label-width="75px" @submit.prevent>
-                <el-form-item label="原文件名">
-                    <el-input v-model="queryParams.originalname" clearable @change="handleQuery" />
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click.stop="handleQuery"> 查询 </el-button>
-                    <el-button> 重置 </el-button>
-                </el-form-item>
-            </el-form>
-        </template>
-        <Table v-model:reload="reload" :columns="columns" :request-api="fetchUploadInfos" :request-params="queryParams">
-        </Table>
+        <Table :columns="columns" :search="{ labelWidth: 110, options }" :request-api="fetchUploadInfos"> </Table>
     </PageContainer>
 </template>
